949591931441097c7cb81f040b85885e2da28abc
EduardoGomesSa <eduardo.sa.022@gmail.com>
2024-06-13 20:13:40 -0300

    Criação do arquivo UserUpdateRequest

---
+++ app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id'=>'required|numeric',
+            'name'=>'sometimes|required|string',
+            'cpf'=>'sometimes|required|string|unique:users,cpf',
+            'email' => 'sometimes|required|email|unique:users,email',
+            'password'=>'sometimes|required|string',
+            'birth_date'=>'sometimes|required|date',
+        ];
+    }
+}
